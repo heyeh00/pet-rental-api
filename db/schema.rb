bf0bfd74555f4a241e76599af7a99ab9f3a2eeff
Henry Yeh <heyeh@icloud.com>
2022-11-29 13:07:48 +0800

    created Booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_050651) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_050732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "pet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pet_id"], name: "index_bookings_on_pet_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
@@ -35,4 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_050651) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookings", "pets"
+  add_foreign_key "bookings", "users"
 end
