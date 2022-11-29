78c92c84bfe8708ca962cb2780e2891b5c0e2305
Yifei Kong <fifi_yifei@yeah.net>
2022-11-29 14:57:25 +0800

    user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :bookings
   has_many :pets, through: :bookings
+
+  validates :name, presence: true
+  validates :open_id, presence: true, uniqueness: true
+  validates :image_url, presence: true
+  validates :gender, presence: true
+
 end
