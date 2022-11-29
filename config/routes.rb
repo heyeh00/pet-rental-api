4369bcfe51d947558f850ddd4abb687fe5302ae3
Henry Yeh <heyeh@icloud.com>
2022-11-29 14:24:47 +0800

    created pets and user controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      post 'login', to: 'sessions#login', as: :login
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+      resources :users, only: %i[create show] do
+        resources :bookings, only: %i[index create]
+      end
+
+      resources :pets, only: %i[index show create]
+    end
+  end
 end
