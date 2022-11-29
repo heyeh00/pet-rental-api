b4498af10e74bc886f27d229656c237e7d3eab72
Yifei Kong <fifi_yifei@yeah.net>
2022-11-29 18:39:50 +0800

    usercontroller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,23 +1,19 @@
 class Api::V1::UsersController < Api::V1::BaseController
+  skip_before_action :verify_request
+
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path, notice: 'Your account has been successfully created.'
+      render json: { user: @user }
+      # render json:, notice: 'Your account has been successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-
-  def destroy
-    @user = User.find(params[:open_id])
-    @user.destroy
-    redirect_to user_path, notice: 'You have successfully deleted your account.'
-  end
-
   private
 
   def user_params
-    params.require(:user)
+    params.require(:user).permit(:name, :open_id, :image_url, :gender)
   end
 end
