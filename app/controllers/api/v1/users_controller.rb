13784be36fe5078e2b5fe173514faf5ef9ef8ee6
Yifei Kong <fifi_yifei@yeah.net>
2022-11-29 15:46:38 +0800

    users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::UsersController < Api::V1::BaseController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path, notice: 'Your account has been successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @user = User.find(params[:open_id])
+    @user.destroy
+    redirect_to user_path, notice: 'You have successfully deleted your account.'
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+  end
 end
