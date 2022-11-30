2f8c6f1e397ca4d929cc82cc17a169a6f0cb9bb2
Henry Yeh <heyeh@icloud.com>
2022-11-30 10:52:18 +0800

    Fixed User Authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,5 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :pets, through: :bookings
 
-  validates :name, presence: true
   validates :open_id, presence: true, uniqueness: true
-  validates :image_url, presence: true
-  validates :gender, presence: true
-
 end
