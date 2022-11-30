7dae16424067fc7efae7cb38a165501fe61288a2
Henry Yeh <heyeh@icloud.com>
2022-11-30 11:31:24 +0800

    fixed again. github is hard

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,5 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :pets, through: :bookings
 
-  validates :name, :gender, presence: true
-  validates :open_id, presence: true, uniqueness: true
-
+  validates :open_id
 end
